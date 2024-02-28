11ac78a0689d78e4f069a101d839e0ae836d806d
Susa Milán <mailtosnek@protonmail.com>
2024-02-28 18:48:26 +0100

    Server CRUD + User CRUD fix

---
+++ internal/db/server.go
@@ -0,0 +1,26 @@
+package db
+
+import "github.com/busyLambda/bbkk/internal/models"
+
+func (d *DbManager) InsertServer(s models.Server) error {
+	return d.Conn.Create(&s).Error
+}
+
+func (d *DbManager) DeleteServer(id int) error {
+	s, err := d.GetServerByID(id)
+	if err != nil {
+		return err
+	}
+
+	return d.Conn.Delete(&s).Error
+}
+
+func (d *DbManager) GetServerByID(id int) (s models.Server, err error) {
+	err = d.Conn.Where("id = ?").First(&s).Error
+	return
+}
+
+func (d *DbManager) GetServerByName(sn string) (s models.Server, err error) {
+	err = d.Conn.Where("name = ?", sn).First(&s).Error
+	return
+}
