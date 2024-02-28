42e61ce12afa3f7575e0d805456d42361a4ae96c
Susa Milán <mailtosnek@protonmail.com>
2024-02-28 18:48:10 +0100

    User BeforeDelete hook

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"fmt"
+
 	"github.com/busyLambda/bbkk/domain/user"
 	"gorm.io/gorm"
 )
@@ -17,4 +19,11 @@ func NewUser(un user.Username, pw user.Password) User {
 		Username: un,
 		Password: pw,
 	}
+}
+
+func (u *User) BeforeDelete(tx *gorm.DB) (err error) {
+	if u.Role == user.SUPERADMIN {
+		return fmt.Errorf("cannot_delete_superadmin")
+	}
+	return
 }
