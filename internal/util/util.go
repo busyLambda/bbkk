cd726eb5c4e24784033500012c667d7b20c7051a
Susa Milan <mailtosnek@protonmail.com>
2024-03-11 13:15:48 +0100

    hash password fn and registration form

--- internal/util/util.go
+++ internal/util/util.go
@@ -4,24 +4,41 @@ import (
 	"log"
 	"os/exec"
 	"strings"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 // Formats the flags and the jar args to java so that it's executed properly.
 func JavaCmd(dir string, jar string, flags string) *exec.Cmd {
-  e := []string{"-jar", jar}
+	e := []string{"-jar", jar}
 
-  j := strings.ReplaceAll(flags, "\n", " ")
+	j := strings.ReplaceAll(flags, "\n", " ")
 
-  var f []string
-  if j != "" {
-    f = append(strings.Split(j, " "), e...)
-    } else {
-    f = e
-  }
-  log.Printf("CMD_FLAGS: %s\n", f)
+	var f []string
+	if j != "" {
+		f = append(strings.Split(j, " "), e...)
+	} else {
+		f = e
+	}
+	log.Printf("CMD_FLAGS: %s\n", f)
 
-  c := exec.Command("java", f...)
-  c.Dir = dir
+	c := exec.Command("java", f...)
+	c.Dir = dir
 
-  return c
+	return c
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+type RegistrationForm struct {
+	Username string
+	Password string
 }
