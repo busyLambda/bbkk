82c9713a43e71773f6536944fde022d77f9168cf
Susa Milan <mailtosnek@protonmail.com>
2024-03-11 13:16:26 +0100

    Registration form use

--- internal/api/user.go
+++ internal/api/user.go
@@ -5,18 +5,36 @@ import (
 	"net/http"
 
 	"github.com/busyLambda/bbkk/internal/models"
+	"github.com/busyLambda/bbkk/internal/util"
+	"github.com/go-chi/chi"
 )
 
 func (a *App) AddUser(w http.ResponseWriter, r *http.Request) {
-	var u models.User
+	var rf util.RegistrationForm
 
-	err := json.NewDecoder(r.Body).Decode(&u)
+	err := json.NewDecoder(r.Body).Decode(&rf)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	u, err := models.NewUser(rf)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	err = a.db.InsertUser(u)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (a *App) DeleteUser(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+
+	err := a.db.Conn.Delete(models.User{}, id).Error
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
