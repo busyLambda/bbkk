a6e77e91e156dad7105802ffb10090ea8983b1e5
Susa Milán <mailtosnek@protonmail.com>
2024-03-29 19:01:27 +0100

    Properly start the API and add optional port

--- cmd/bbkk/main.go
+++ cmd/bbkk/main.go
@@ -1,42 +1,18 @@
 package main
 
-import "github.com/busyLambda/bbkk/internal/api"
+import (
+	"log"
+
+	"github.com/busyLambda/bbkk/internal/api"
+)
 
 func main() {
 	app := api.NewApiMaster()
 
 	app.AttachRoutes()
 
-	app.Run()
-	/*
-		var wg sync.WaitGroup
+	port := 3000
 
-		mcServer := server.NewMcServer("server", "paper.jar", "")
-
-		wg.Add(1)
-		go mcServer.Start(&wg)
-
-		outchan := make(chan string)
-
-		if mcServer.Stdout == nil {
-			fmt.Printf("No stdout, setting it...\n")
-			mcServer.SetStdout()
-		}
-
-		go mcServer.ReadStdout(outchan)
-
-		go func() {
-			for {
-				select {
-				case data := <-outchan:
-					fmt.Printf(data)
-				}
-			}
-		}()
-
-		time.Sleep(1 * time.Second)
-		mcServer.WriteString("stop\n")
-
-		wg.Wait()
-	*/
+	log.Printf("Server is running on port %d", port)
+	app.Run(3000)
 }
